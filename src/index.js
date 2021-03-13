2a6e04b0a372ed005d77899379fbb502471a3deb
AlenaVP <poprugo@gmail.com>
2021-03-13 10:20:06 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+let tillTeens = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+let dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+function isTenn(number) {
+  return ('' + number).length === 2 && number < 20 ? true : false;
+}
+
+function isDozen(number) {
+  return ('' + number).length === 2 && number >= 20 ? true : false;
+}
+
+function isHundred(number) {
+  return ('' + number).length === 3 ? true : false;
+}
+
+function teen(number) {
+  return tillTeens[number];
+}
+
+function dozen(number) {
+  return dozens[Math.floor(number / 10)] + (number % 10 === 0 ? '' : ' ' + teen(number % 10));
+}
+
+function hundred(number) {
+  return tillTeens[Math.floor(number / 100)] + ' hundred' +
+    (number % 100 === 0 ? '' : ' ' + (isDozen(number % 100) ? dozen(number % 100) : teen(number % 100)));
+}
+
 module.exports = function toReadable (number) {
-  
+  return isHundred(number) ? hundred(number) : (isDozen(number) ? dozen(number) : (number != 0 ? teen(number) : 'zero'));
 }
