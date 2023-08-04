36a9d005d6a813cbeb7e80ab0fba41844dd283cf
AlenaVP <poprugo@gmail.com>
2023-08-04 15:02:50 +0300

    refactor: change ternary to if-else with recursion

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
-let tillTeens = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+let tillTeens = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
 let dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-function isDozen(number) {
-  return ('' + number).length === 2 && number >= 20 ? true : false;
-}
-
-function isHundred(number) {
-  return ('' + number).length === 3 ? true : false;
-}
-
-function dozen(number) {
-  return dozens[Math.floor(number / 10)] + (number % 10 != 0 ? ' ' + tillTeens[number % 10] : '');
-}
-
-function hundred(number) {
-  return tillTeens[Math.floor(number / 100)] + ' hundred' +
-    (number % 100 != 0 ? ' ' + (isDozen(number % 100) ? dozen(number % 100) : tillTeens[number % 100]) : '');
-}
-
-module.exports = function toReadable (number) {
-  return isHundred(number) ? hundred(number) : (isDozen(number) ? dozen(number) : (number != 0 ? tillTeens[number] : 'zero'));
+module.exports = function toReadable(number) {
+  str = '';
+  if (number < 20) {
+    str += tillTeens[number];
+  } else if (('' + number).length === 2) {
+    str += dozens[Math.floor(number / 10)] + (number % 10 != 0 ? ' ' + toReadable(number % 10) : '');
+  } else if (('' + number).length === 3) {
+    str += tillTeens[Math.floor(number / 100)] + ' hundred' + (number % 100 != 0 ? ' ' + toReadable(number % 100) : '');
+  }
+  return str;
 }
